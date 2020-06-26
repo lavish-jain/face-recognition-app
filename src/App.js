5e2daa0399292030e344fd4a332eebdd97d69e32
lavish-jain <lavishrjain1997@gmail.com>
2020-06-26 08:04:21 +0530

    Added Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/* <Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
