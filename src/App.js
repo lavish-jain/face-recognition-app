1df9b45964a835e99dff7e3c7749791da210aa16
lavish-jain <lavishrjain1997@gmail.com>
2020-06-28 09:14:01 +0530

    Added Rank Component. Added ParticleJs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enabled: true,
+        value_area: 800,
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Particles
+          params={particleOptions}
+          className="particles"
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Rank extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className='white f3'>
+                    {'Lavish, your current rank is...'}
+                </div>
+                <div className='white f1'>
+                    {'#5'}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Rank;
