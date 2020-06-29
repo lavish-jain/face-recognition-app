ea14215c600a0a3a4de2997d83d4e0270286c7cd
lavish-jain <lavishrjain1997@gmail.com>
2020-06-29 09:11:04 +0530

    Fixed react warnings

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -4,17 +4,17 @@ class Signin extends React.Component {
     render() {
         const { onRouteChange } = this.props;
         return (
-            <article class="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
-                    <form className="measure">
+                    <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
-                                <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
                             </div>
                             <div className="mv3">
-                                <label className="db fw6 lh-copy f6" for="password">Password</label>
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                 <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
                             </div>
                         </fieldset>
@@ -24,7 +24,7 @@ class Signin extends React.Component {
                         <div className="lh-copy mt3">
                             <p className="f6 link dim black db pointer" onClick={() => onRouteChange('register')}>Register</p>
                         </div>
-                    </form>
+                    </div>
                 </main >
             </article>
         )
